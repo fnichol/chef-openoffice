729ebe542a939190188973b0bccef59550b07c96
Fletcher Nichol <fnichol@nichol.ca>
2010-12-11 15:29:35 -0700

    Add headless recipe.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,11 @@ license          "Apache 2.0"
 description      "Installs/Configures openoffice"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+recipe "openoffice", "Default, empty recipe"
+recipe "openoffice::headless", "Headless install of OpenOffice"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
