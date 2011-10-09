e2edd488d72cd1248c1551e528aaa888fca9b4dd
Fletcher Nichol <fnichol@nichol.ca>
2011-10-09 13:43:57 -0600

    Fix metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,12 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures openoffice"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
 
-recipe "openoffice", "Default, empty recipe"
-recipe "openoffice::headless", "Headless install of OpenOffice"
+recipe "openoffice",            "Default, empty recipe"
+recipe "openoffice::headless",  "Installs the headless (no X) core of OpenOffice."
+recipe "openoffice::apps",      "Installs the application components for OpenOffice, namely Writer, Calc, and Impress."
 
 supports "ubuntu"
 
