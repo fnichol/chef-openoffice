73c025556d0d3e42a03e4c2fa66bfafad8f7c4af
Fletcher Nichol <fnichol@nichol.ca>
2011-10-09 13:26:27 -0600

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,11 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures openoffice"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "0.1.1"
 
 recipe "openoffice", "Default, empty recipe"
 recipe "openoffice::headless", "Headless install of OpenOffice"
 
-%w{ ubuntu }.each do |os|
-  supports os
-end
+supports "ubuntu"
 
+depends "java"
